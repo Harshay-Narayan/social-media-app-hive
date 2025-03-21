e34341ca20c950747ebf81b2102442712abcdfe8
harshay560 <harshaynarayan494@gmail.com>
2025-03-21 19:32:32 +0530

    socket url reference fixed

--- src/config/conf.ts
+++ src/config/conf.ts
@@ -6,3 +6,7 @@ export const pusherConfig = {
 export const redisConfig = {
   redisUrl: String(process.env.REDIS_URL),
 };
+
+export const socketConfig = {
+  socketUrl: String(process.env.EXT_PUBLIC_SOCKET_URL),
+};
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,5 @@
 "use client";
+import { socketConfig } from "@/config";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+export const socket = io(socketConfig.socketUrl);
